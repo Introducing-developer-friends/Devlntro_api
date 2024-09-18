bbd9d102c9a31acea2e890dcac49007b1e281afd
doyeon kim <kim2004on@gmail.com>
2024-09-19 03:13:48 +0900

    Update: 피드 모듈 수정 게시물 상세조회에 댓글 좋아요 추가

--- src/feed/feed.module.ts
+++ src/feed/feed.module.ts
@@ -8,9 +8,9 @@ import { Post } from '../entities/post.entity';
 import { BusinessContact } from '../entities/business-contact.entity'; // BusinessContact 엔티티 임포트
 import { Comment } from '../entities/comment.entity'; // Comment 엔티티 임포트
 import { PostLike } from '../entities/post-like.entity'; // PostLike 엔티티 임포트
-
+import { CommentLike } from '../entities/comment-like.entity';
 @Module({
-  imports: [TypeOrmModule.forFeature([Post, BusinessContact, Comment, PostLike])], // Post와 BusinessContact 엔티티 등록
+  imports: [TypeOrmModule.forFeature([Post, BusinessContact, Comment, PostLike, CommentLike])], // Post와 BusinessContact 엔티티 등록
   controllers: [FeedController],
   providers: [FeedService, FeedFilterService, SortingService], // FeedFilterService와 SortingService 등록
 })
