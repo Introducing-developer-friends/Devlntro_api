fcf047d6e838a0408044f1ea989b2d9b875c7199
doyeon kim <kim2004on@gmail.com>
2024-09-21 00:42:22 +0900

    Feat: 인맥 관계 소프트 삭제 구현 - 엔티티 변경 및 마이그레이션 진행

--- src/entities/business-contact.entity.ts
+++ src/entities/business-contact.entity.ts
@@ -1,16 +1,22 @@
-import { Entity, PrimaryGeneratedColumn, ManyToOne, JoinColumn, CreateDateColumn } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, ManyToOne, JoinColumn, CreateDateColumn, DeleteDateColumn } from 'typeorm';
 import { UserAccount } from './user-account.entity';
 
 @Entity()
 export class BusinessContact {
-  @PrimaryGeneratedColumn() // 자동으로 증가하는 기본 키
+  @PrimaryGeneratedColumn()
   contact_id: number;
 
   @ManyToOne(() => UserAccount, userAccount => userAccount.contacts)
-  @JoinColumn({ name: 'user_id' }) // 외래 키인 user_id로 연결
+  @JoinColumn({ name: 'user_id' })
   userAccount: UserAccount;
 
   @ManyToOne(() => UserAccount, contact_user => contact_user.contactOf)
-  @JoinColumn({ name: 'contact_user_id' }) 
+  @JoinColumn({ name: 'contact_user_id' })
   contact_user: UserAccount;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @DeleteDateColumn()
+  deleted_at: Date;
 }
