1232788af60561fb4cbd548af5cbc3b34cfb12ac
doyeon kim <kim2004on@gmail.com>
2024-10-07 19:09:12 +0900

    Perf: Post 엔티티 - user_id, created_at 인덱스 추가 - user_id : 조회할 때 user_id로 하기 때문에 추가 - 게시물을 시간순으로 정렬하는게 기본값이기때문에 추가

--- src/entities/post.entity.ts
+++ src/entities/post.entity.ts
@@ -1,10 +1,11 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany, JoinColumn, CreateDateColumn, DeleteDateColumn  } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany, JoinColumn, CreateDateColumn, DeleteDateColumn, Index   } from 'typeorm';
 import { UserAccount } from './user-account.entity';
 import { Comment } from './comment.entity';
 import { PostLike } from './post-like.entity';
 
 // Post 엔티티는 사용자가 작성한 게시물을 나타냅니다.
 @Entity()
+@Index(["user", "created_at"])
 export class Post {
   // post_id는 기본 키로 자동 생성.
   @PrimaryGeneratedColumn()
@@ -12,6 +13,7 @@ export class Post {
 
   @ManyToOne(() => UserAccount, user => user.posts) // 작성자와의 다대일 관계
   @JoinColumn({ name: 'user_id' }) // 외래 키 컬럼을 user_id로 지정
+  @Index()
   user: UserAccount;
 
   @Column()
@@ -23,6 +25,7 @@ export class Post {
 
   // 게시물이 생성된 날짜 및 시간을 자동으로 저장
   @CreateDateColumn()
+  @Index()
   created_at: Date;
 
   @DeleteDateColumn() 
