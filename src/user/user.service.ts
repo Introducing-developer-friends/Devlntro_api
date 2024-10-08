79853700d98e42f84c5c7ad742369af26c35ff5a
doyeon kim <kim2004on@gmail.com>
2024-10-08 15:53:55 +0900

    Perf: 유저 정보 수정 관련 로직 개선 - 트랜잭션 적용하여 여러 db작업을 하나의 원자적 작업으로 처리하여 데이터 일관성 보장 - 회원탈퇴 엔드포인트의 경로 수정 - 회원 탈퇴시 비즈니스 프로필도 같이 삭제되게끔 수정

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,7 @@
 // user.service.ts
 import { Injectable, NotFoundException, BadRequestException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, DataSource  } from 'typeorm';
 import { UserAccount } from '../entities/user-account.entity';
 import { BusinessProfile } from '../entities/business-profile.entity';
 import { UpdateBusinessProfileDto } from './dto/update-business-profile.dto';
@@ -20,11 +20,16 @@ export class UserService {
     // BusinessProfile 리포지토리를 주입
     @InjectRepository(BusinessProfile)
     private businessProfileRepository: Repository<BusinessProfile>,
+    private dataSource: DataSource
   ) {}
 
   // 비즈니스 프로필 업데이트 메서드
   async updateBusinessProfile(userId: number, updateProfileDto: UpdateBusinessProfileDto) {
-    const user = await this.userAccountRepository.findOne({ where: { user_id: userId }, relations: ['profile'] });
+    return this.dataSource.manager.transaction(async transactionalEntityManager => {
+      const user = await transactionalEntityManager.findOne(UserAccount, { 
+        where: { user_id: userId },
+        relations: ['profile']
+      });
     
     // 사용자가 없으면 예외 발생
     if (!user) {
@@ -34,36 +39,35 @@ export class UserService {
     // UserAccount 업데이트 (이름 필드만)
     if (updateProfileDto.name) {
         user.name = updateProfileDto.name;
-        await this.userAccountRepository.save(user);
+        await transactionalEntityManager.save(UserAccount, user);
       }
   
       // BusinessProfile 업데이트
       if (!user.profile) {
-        const newProfile = this.businessProfileRepository.create({
+        const newProfile = transactionalEntityManager.create(BusinessProfile, {
           ...updateProfileDto,
           userAccount: user
         });
-        await this.businessProfileRepository.save(newProfile);
+        await transactionalEntityManager.save(BusinessProfile, newProfile);
       } else {
-        // 이름 필드를 제외한 나머지 필드만 업데이트
         const { name, ...profileUpdateData } = updateProfileDto;
         Object.assign(user.profile, profileUpdateData);
-        await this.businessProfileRepository.save(user.profile);
+        await transactionalEntityManager.save(BusinessProfile, user.profile);
       }
 
-    // 변경된 사용자 정보를 저장
-    await this.userAccountRepository.save(user);
+
     return {
       statusCode: 200,
       message: "프로필 정보가 성공적으로 수정되었습니다."
     };
+    });
   }
 
   // 비밀번호 변경 메서드
   async changePassword(userId: number, changePasswordDto: ChangePasswordDto) {
-    
+    return this.dataSource.manager.transaction(async transactionalEntityManager => {
     // 주어진 userId로 사용자를 찾음
-    const user = await this.userAccountRepository.findOne({ where: { user_id: userId } });
+    const user = await transactionalEntityManager.findOne(UserAccount, { where: { user_id: userId } });
     
     // 사용자가 없으면 예외 발생
     if (!user) {
@@ -81,16 +85,21 @@ export class UserService {
     }
 
     user.password = await bcrypt.hash(changePasswordDto.newPassword, 10);
-    await this.userAccountRepository.save(user);
+    await transactionalEntityManager.save(UserAccount, user);
     
     return {
       statusCode: 200,
       message: "비밀번호가 성공적으로 변경되었습니다." // 성공 메시지 반환
     };
+  });
   }
 
   async deleteAccount(userId: number, deleteAccountDto: DeleteAccountDto) {
-    const user = await this.userAccountRepository.findOne({ where: { user_id: userId } });
+    return this.dataSource.manager.transaction(async transactionalEntityManager => {
+      const user = await transactionalEntityManager.findOne(UserAccount, { 
+        where: { user_id: userId },
+        relations: ['profile']
+      });
 
     if (!user) {
       throw new NotFoundException('User not found');
@@ -101,13 +110,19 @@ export class UserService {
       throw new UnauthorizedException('비밀번호가 올바르지 않습니다.');
     }
 
+    // 비즈니스 프로필 소프트 삭제
+    if (user.profile) {
+      await transactionalEntityManager.softDelete(BusinessProfile, user.profile.profile_id);
+    }
+
     // 소프트 삭제 실행
-    await this.userAccountRepository.softDelete(userId);
+    await transactionalEntityManager.softDelete(UserAccount, userId);
 
     return {
       statusCode: 200,
       message: "회원 탈퇴가 성공적으로 처리되었습니다."
     };
+  });
   }
 
 
