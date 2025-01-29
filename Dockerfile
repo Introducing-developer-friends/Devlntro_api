e14ab6b449313e1eed241859fca0087b887e53b3
doyeon kim <kim2004on@gmail.com>
2025-01-29 16:27:34 +0900

    Chore: DB_USERNAME 시크릿 변수로 변경 및 불필요한 주석 제거

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,8 @@
-# Node.js 20.15.0 버전 사용
 FROM node:20.15.0
-
-# 작업 디렉토리 설정
 WORKDIR /app
-
-# package.json과 package-lock.json 복사
 COPY package*.json ./
-
-# 의존성 설치
 RUN npm install
-
-# 소스 코드 복사
 COPY . .
-
 RUN npm run build
-
-# 포트 설정 (NestJS 기본 포트는 3000입니다)
 EXPOSE 3000
-
 CMD sh -c "sleep 20 &&NODE_ENV=production npm run migration:run:prod && node dist/main.js"
--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,12 @@ import { AppModule } from './app.module';
 import { seedInitialData } from './seeds/initial-data.seed';
 import { DataSource } from 'typeorm';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common'; // 유효성 검사 파이프 추가
-import { HttpExceptionFilter } from './common/filters/http-exception.filter'; // 전역 에러 필터 추가
+import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // CORS 설정
   app.enableCors({
     origin:
       /^(http:\/\/localhost:5173|https:\/\/d2u27gghld24cr\.cloudfront\.net)$/,
@@ -20,18 +19,16 @@ async function bootstrap() {
   if (process.env.NODE_ENV === 'production') {
     app.setGlobalPrefix('api');
   }
-  // 전역 유효성 검사 파이프 설정
+
   app.useGlobalPipes(new ValidationPipe());
 
-  // 전역 에러 필터 설정
   app.useGlobalFilters(new HttpExceptionFilter());
 
-  // Swagger 설정 추가
   const config = new DocumentBuilder()
     .setTitle('API Documentation')
     .setDescription('API documentation for the project')
     .setVersion('1.0')
-    .addBearerAuth() // JWT 인증 추가
+    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
@@ -40,8 +37,6 @@ async function bootstrap() {
       persistAuthorization: true,
     },
   });
-
-  // // 데이터 시드 삽입2
 
   const dataSource = app.get(DataSource);
   await seedInitialData(dataSource);
